078b7eb49aa31005ecfdaea9d02d878837bd9bc6
Mohd Imtiaz <mohdimtiaz.uddin@highspot.com>
2025-01-19 21:49:03 +0530

    Updated login screen

--- client/src/sections/Login/Login.tsx
+++ client/src/sections/Login/Login.tsx
@@ -6,7 +6,11 @@ import { Checkbox } from "../../components/Checkbox";
 
 const Login = () => {
   return (
-    <div className="mt-16 p-8 max-w-screen-sm flex flex-col gap-2 mx-auto">
+    <div className="mt-16 p-8 max-w-sm flex flex-col gap-2 mx-auto">
+      <div className="my-4">
+        <div className="text-center text-2xl font-thin md:text-3xl">Log in to your account</div>
+        <div className="text-center text-sm text-gray-500 font-thin">Welcome back! Please enter your details</div>
+      </div>
       <Input name="email" label="Email" type={InputTypes.email} placeholder="Enter email address" onChange={() => {}} />
       <Input
         name="password"
@@ -18,7 +22,7 @@ const Login = () => {
 
       <div className="flex justify-between items-center text-sm">
         <Checkbox name="rememberMe" label="Remember Me" disabled={false} size={Size.sm} />
-        <Button size={Size.sm} type={ButtonType.link} onClick={() => {}}>
+        <Button size={Size.sm} type={ButtonType.link} onClick={() => {}} className="ps-0 pe-0">
           Forgot Password
         </Button>
       </div>
@@ -28,8 +32,8 @@ const Login = () => {
       </Button>
 
       <div className="flex justify-center items-center text-sm">
-        <span>Dont' have an account? </span>
-        <Button size={Size.sm} type={ButtonType.link} onClick={() => {}}>
+        <span>Don't have an account? </span>
+        <Button size={Size.sm} type={ButtonType.link} onClick={() => {}} className="ps-1 pe-1">
           Sign up
         </Button>
       </div>
