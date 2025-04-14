ec5d30a81a7e4088d7369707250f63c2ac000587
Mohd Imtiaz <imtiyaz.masodi@gmail.com>
2025-04-14 15:25:33 +0530

    Added routing using react-router-dom

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,26 @@
-import { Login } from "./sections/Login";
+import { lazy, Suspense } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+
+const Home = lazy(() => import("./pages/Home/Home"));
+const Login = lazy(() => import("./pages/Login/Login"));
 
 function App() {
   return (
-    <>
-      <Login />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" index element={<Home />} />
+        <Route
+          path="/login"
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <Login />
+            </Suspense>
+          }
+        />
+        <Route path="*" element={<div>404 Not Found</div>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
