e07c53f7bd3c7f97bfeac82fc42c78c6be27bcde
Mohd Imtiaz <imtiyaz.masodi@gmail.com>
2025-04-14 12:21:53 +0530

    Added Pulse and Circular Loader components

---
+++ client/src/components/Loader/CircularLoader/CircularLoader.tsx
@@ -0,0 +1,8 @@
+const CircularLoader = () => {
+  // ToDo: Add Size variants
+  return (
+    <div className="h-full min-h-6 max-h-32 aspect-square border-4 rounded-full border-t-4 border-t-[currentColor] m-auto animate-spin"></div>
+  );
+};
+
+export default CircularLoader;
---
+++ client/src/components/Loader/PulseLoader/PulseLoader.tsx
@@ -0,0 +1,15 @@
+import "./PulseLoader.css";
+
+const PulseLoader = () => {
+  return (
+    <div className="flex justify-center items-center gap-1">
+      <div className="dot dot1" />
+      <div className="dot dot2" />
+      <div className="dot dot3" />
+      <div className="dot dot4" />
+      <div className="dot dot5" />
+    </div>
+  );
+};
+
+export default PulseLoader;
