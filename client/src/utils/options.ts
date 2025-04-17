ab0e337450c47f367ba50d1d330ff50a07d7b07f
Mohd Imtiaz <imtiyaz.masodi@gmail.com>
2025-04-18 00:28:05 +0530

    Updated priority options

--- client/src/utils/options.ts
+++ client/src/utils/options.ts
@@ -1,6 +1,7 @@
+import { PriorityType } from "../types/PriorityType";
 import { Priority } from "./enums";
 
-export const priorityOptions = Object.freeze([
+export const priorityOptions: readonly PriorityType[] = Object.freeze([
   { label: "Low", value: Priority.low },
   { label: "Medium", value: Priority.medium },
   { label: "High", value: Priority.high },
