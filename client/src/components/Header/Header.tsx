7e0614d8bb577c4c0a35b17654573824b733524e
Mohd Imtiaz <imtiyaz.masodi@gmail.com>
2025-04-18 12:46:14 +0530

    Updated header gap ui changes

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -16,12 +16,12 @@ function Header() {
   }, [location]);
 
   return (
-    <div className="h-16 px-8 py-2 fixed top-0 left-0 w-full flex items-center justify-between bg-white border-b border-b-zinc-200 z-10">
-      <div className="flex items-center gap-4">
+    <div className="h-16 px-6 md:px-8 py-2 fixed top-0 left-0 w-full flex items-center justify-between bg-white border-b border-b-zinc-200 z-10">
+      <div className="flex items-center">
         <Icon
           name={Icons.arrowBack}
           className={`text-2xl text-gray-700 cursor-pointer ${
-            showBack ? "w-6 text-inherit" : "w-0 text-transparent"
+            showBack ? "w-6 text-inherit mr-2 md:mr-4" : "w-0 text-transparent mr-0"
           } transition-all`}
           onClick={() => navigate(-1)}
         />
