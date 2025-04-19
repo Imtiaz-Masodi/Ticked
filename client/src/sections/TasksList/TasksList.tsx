77edd726dd3cf85608eb91352c9c127566a634fe
Mohd Imtiaz <imtiyaz.masodi@gmail.com>
2025-04-19 19:26:10 +0530

    Made task item swipeable

--- client/src/sections/TasksList/TasksList.tsx
+++ client/src/sections/TasksList/TasksList.tsx
@@ -1,3 +1,4 @@
+import { Swipeable } from "../../components/Swipeable";
 import { TaskItem } from "../../components/TaskItem";
 import { useGetTasksQuery } from "../../store/api/taskApi";
 import { Task } from "../../types/Task";
@@ -10,7 +11,22 @@ function TasksList() {
     <div className="max-w-screen-md mx-auto flex flex-col items-center justify-center px-2 gap-2">
       <p className="text-sm font-bold text-zinc-600 self-start mx-2">Todo</p>
       {tasks.map((task: Task) => (
-        <TaskItem key={task._id} task={task} />
+        <Swipeable
+          className="rounded-md"
+          swipingLeftBgContent={
+            <div className="bg-sky-500 h-full text-right flex justify-end items-center px-4 text-white">
+              Mark as Complete
+            </div>
+          }
+          swipingRightBgContent={
+            <div className="bg-rose-500 h-full flex justify-start items-center px-4 text-white">Archive</div>
+          }
+          onSwipeLeft={() => {}}
+          onSwipeRight={() => {}}
+          key={task._id}
+        >
+          <TaskItem task={task} />
+        </Swipeable>
       ))}
     </div>
   );
