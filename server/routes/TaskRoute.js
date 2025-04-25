7972154441141cfbb6b160f2769d00dcf973d307
Mohd Imtiaz <imtiyaz.masodi@gmail.com>
2025-04-25 17:56:43 +0530

    Updated task routes

--- server/routes/TaskRoute.js
+++ server/routes/TaskRoute.js
@@ -1,21 +1,25 @@
 const express = require("express");
 const { authentication } = require("../middlewares/authentication");
 const { validateTaskSchema } = require("../middlewares/validateTaskSchema");
-const { validateTaskQueryParams, validateTaskIdToDelete } = require("../middlewares/validateTaskRequest");
+const {
+  validateTaskQueryParams,
+  validateTaskId,
+} = require("../middlewares/validateTaskRequest");
 const {
   fetchTasksForUser,
   createTask,
-  markTaskAsComplete,
   updateTask,
-  deleteTask,
+  updateTaskStatus,
+  updateDeleteStatusTo,
 } = require("../controllers/TaskController");
 const router = express.Router();
 
 router.use(authentication);
 router.get("/list", validateTaskQueryParams, fetchTasksForUser);
 router.post("/create", validateTaskSchema, createTask);
-router.put("/update", validateTaskSchema, updateTask);
-router.put("/mark-complete", markTaskAsComplete);
-router.delete("/delete/:taskId", validateTaskIdToDelete, deleteTask);
+router.put("/update/:taskId", validateTaskId(), validateTaskSchema, updateTask);
+router.put("/update-status/:taskId", validateTaskId(), updateTaskStatus);
+router.put("/restore/:taskId", validateTaskId(true), updateDeleteStatusTo(false));
+router.delete("/delete/:taskId", validateTaskId(true), updateDeleteStatusTo(true));
 
 module.exports = router;
