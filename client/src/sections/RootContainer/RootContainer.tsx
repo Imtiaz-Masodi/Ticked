4503cf8eefb0c3cd9275c919e15392b7f067ef4d
Mohd Imtiaz <imtiyaz.masodi@gmail.com>
2025-06-09 14:01:03 +0530

    Implemented navigation drawer in root component

--- client/src/sections/RootContainer/RootContainer.tsx
+++ client/src/sections/RootContainer/RootContainer.tsx
@@ -1,11 +1,23 @@
 import { Outlet } from "react-router-dom";
+import { useState } from "react";
 import Header from "../../components/Header/Header";
+import { NavigationDrawer } from "../../components/NavigationDrawer";
 
 function RootContainer() {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+  const handleToggleDrawer = () => {
+    setIsDrawerOpen((prev) => !prev);
+  };
+
   return (
     <div id="root-container" className="min-h-screen bg-zinc-50">
-      <Header />
-      <div className="py-16 container mx-auto">
+      <Header onMenuIconClick={handleToggleDrawer} />
+      <NavigationDrawer
+        isOpen={isDrawerOpen}
+        onClose={() => setIsDrawerOpen(false)}
+      />
+      <div className="py-16 md:pl-64 container mx-auto transition-all duration-300">
         <Outlet />
       </div>
     </div>
