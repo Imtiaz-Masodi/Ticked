4ab480c76a6a641f0692049c7b6c44263d8e6b8f
Mohd Imtiaz <imtiyaz.masodi@gmail.com>
2025-06-23 22:58:48 +0530

    Updated header component to show the hamburger icon only for mobile devices

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 
 import { Size } from "../../utils/enums";
+import { useMobileDetect } from "../../hooks/useMediaQuery";
 import { AppLogo } from "../AppLogo";
 import { Icon } from "../Icon";
 import { Icons } from "../Icon/IconMap";
@@ -14,24 +15,30 @@ function Header({ onMenuIconClick }: HeaderProps) {
   const location = useLocation();
   const navigate = useNavigate();
   const [showBack, setShowBack] = useState(false);
+  const isMobile = useMobileDetect();
 
   useEffect(() => {
     setShowBack(location.pathname !== "/");
   }, [location]);
+
+  // Show hamburger icon only on mobile when on home page, or show back icon on other pages
+  const shouldShowIcon = showBack || (location.pathname === "/" && isMobile);
 
   return (
     <div
       className={`h-16 px-4 md:px-8 py-2 fixed top-0 left-0 w-full flex items-center justify-between bg-white border-b border-b-zinc-200 z-10 transition-all`}
     >
       <div className="flex items-center">
-        <Icon
-          name={showBack ? Icons.arrowBack : Icons.menu}
-          className={`text-2xl text-gray-700 cursor-pointer w-6 text-inherit mr-3 md:mr-4`}
-          onClick={() => {
-            if (showBack) navigate(-1);
-            else onMenuIconClick();
-          }}
-        />
+        {shouldShowIcon && (
+          <Icon
+            name={showBack ? Icons.arrowBack : Icons.menu}
+            className={`text-2xl text-gray-700 cursor-pointer w-6 text-inherit mr-3 md:mr-4`}
+            onClick={() => {
+              if (showBack) navigate(-1);
+              else onMenuIconClick();
+            }}
+          />
+        )}
         <AppLogo className="text-white" size={Size.sm} />
       </div>
       <Icon name={Icons.notification} className="text-2xl" />
