a8bc67826e8398d40f440d5469f7b681ecf55b6f
Mohd Imtiaz <imtiyaz.masodi@gmail.com>
2025-07-27 13:39:51 +0530

    Updated navigation items position

--- client/src/utils/navigationConfig.ts
+++ client/src/utils/navigationConfig.ts
@@ -3,8 +3,8 @@ import { Icons } from "../components/Icon/IconMap";
 
 export const NAV_ITEMS: readonly NavigationItem[] = Object.freeze([
   { name: "All Tasks", path: "/", icon: Icons.task },
-  { name: "Categories", path: "/categories", icon: Icons.category },
   { name: "Backlogs", path: "/backlogs", icon: Icons.archive },
   { name: "Completed", path: "/completed", icon: Icons.taskDone },
+  { name: "Categories", path: "/categories", icon: Icons.category },
   { name: "Settings", path: "/settings", icon: Icons.settings },
 ]);
