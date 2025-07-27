c618b895e58b1a34274bbf1d7061257ac09f7584
Mohd Imtiaz <imtiyaz.masodi@gmail.com>
2025-07-27 13:56:05 +0530

    Added create task nav item

--- client/src/utils/navigationConfig.ts
+++ client/src/utils/navigationConfig.ts
@@ -2,6 +2,7 @@ import { NavigationItem } from "../components/NavigationDrawer";
 import { Icons } from "../components/Icon/IconMap";
 
 export const NAV_ITEMS: readonly NavigationItem[] = Object.freeze([
+  { name: "Create Task", path: "/task/new", icon: Icons.newTask },
   { name: "All Tasks", path: "/", icon: Icons.task },
   { name: "Backlogs", path: "/backlogs", icon: Icons.archive },
   { name: "Completed", path: "/completed", icon: Icons.taskDone },
