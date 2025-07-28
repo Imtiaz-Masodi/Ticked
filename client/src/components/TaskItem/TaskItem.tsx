97424512eb7b5e61343b2c62ac6808927003fbbc
Mohd Imtiaz <imtiyaz.masodi@gmail.com>
2025-07-29 00:29:41 +0530

    Added right arrow icon to task item

--- client/src/components/TaskItem/TaskItem.tsx
+++ client/src/components/TaskItem/TaskItem.tsx
@@ -3,6 +3,8 @@ import { useGetCategoriesQuery } from "../../store/api/categoryApi";
 import { Task } from "../../types/Task";
 import { priorityColorMap } from "../../utils/options";
 import { SkeletonBox } from "../Skeleton";
+import { Icon } from "../Icon";
+import { Icons } from "../Icon/IconMap";
 
 type TaskItemProps = React.PropsWithChildren<{
   task: Task;
@@ -13,7 +15,7 @@ function TaskItem({ task }: TaskItemProps) {
   const category = data?.payload?.categories.find((category) => category._id === task.categoryId) || null;
 
   return (
-    <div className="task-item-container w-full px-4 py-1.5 bg-white shadow-sm flex items-stretch gap-4 rounded-md overflow-hidden">
+    <div className="task-item-container group w-full px-4 py-1.5 bg-white shadow-sm flex items-stretch gap-4 rounded-md overflow-hidden hover:cursor-pointer">
       <div
         className="w-1 flex -ms-4 -my-1.5 flex-shrink-0"
         style={{ backgroundColor: priorityColorMap[task.priority] }}
@@ -31,6 +33,9 @@ function TaskItem({ task }: TaskItemProps) {
           )}
           <p className="text-xs text-zinc-700">{getUserFriendlyDate(task.dueDate)}</p>
         </div>
+      </div>
+      <div className="flex items-center">
+        <Icon name={Icons.right} className="text-zinc-700 text-xl transition-transform duration-200 ease-in-out group-hover:translate-x-1" />
       </div>
     </div>
   );
