54c6fe5a88dfa56d264d43708d3a46a1b952281f
Mohd Imtiaz <imtiyaz.masodi@gmail.com>
2025-07-31 22:46:22 +0530

    Increased category name max length to 30 chars

--- server/schemas/categorySchema.js
+++ server/schemas/categorySchema.js
@@ -2,7 +2,7 @@ const Joi = require("joi");
 const constants = require("../utils/constants");
 
 const categorySchema = Joi.object({
-  categoryName: Joi.string().min(3).max(18).required().label("Category Name").messages({
+  categoryName: Joi.string().min(3).max(30).required().label("Category Name").messages({
     "any.required": "{{#label}} is a mandatory field and cannot be left blank.",
     "string.min": "{{#label}} must be at least {{#limit}} characters long.",
     "string.max": "{{#label}} cannot be more than {{#limit}} characters long.",
