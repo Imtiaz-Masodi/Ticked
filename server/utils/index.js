fa000d3a6ee823ef35b43fa7ea38c90b5d96bdc6
Mohd Imtiaz <imtiyaz.masodi@gmail.com>
2025-08-01 10:05:31 +0530

    Replaced send.status with sendStatus function

--- server/utils/index.js
+++ server/utils/index.js
@@ -8,7 +8,7 @@ function handleCors(req, res, next) {
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, X-Requested-With");
 
   if ("OPTIONS" === req.method) {
-    res.status(200).send(200);
+    res.sendStatus(200);
   } else {
     next();
   }
