927d5fffde22fb9866e7d57b1a75ff31feb5dd42
Mohd Imtiaz <imtiyaz.masodi@gmail.com>
2025-08-04 00:21:11 +0530

    Added fullscreen mode to PageLoader

--- client/src/components/Loader/PageLoading/PageLoading.tsx
+++ client/src/components/Loader/PageLoading/PageLoading.tsx
@@ -4,11 +4,12 @@ import { Size } from "../../../utils/enums";
 interface PageLoadingProps {
   text?: string;
   size?: Size;
+  fullScreen?: boolean;
 }
 
-const PageLoading = ({ text = "Loading...", size = Size.lg }: PageLoadingProps) => {
+const PageLoading = ({ text = "Loading...", size = Size.lg, fullScreen = true }: PageLoadingProps) => {
   return (
-    <div className="w-full h-full flex flex-col items-center justify-center my-8">
+    <div className={`w-full ${fullScreen ? "h-[calc(100vh-64px)]" : "h-full"} flex flex-col items-center justify-center my-8`}>
       <CircularLoader size={size} className="text-blue-500" />
       <p className="mt-4 text-gray-600 dark:text-gray-400 text-sm font-medium">{text}</p>
     </div>
