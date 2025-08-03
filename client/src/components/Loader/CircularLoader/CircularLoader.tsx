7dcd9532ca66523e1be0365f7792b84f2918fa6b
Mohd Imtiaz <imtiyaz.masodi@gmail.com>
2025-08-03 21:25:49 +0530

    Added size variants in CircularLoader component

--- client/src/components/Loader/CircularLoader/CircularLoader.tsx
+++ client/src/components/Loader/CircularLoader/CircularLoader.tsx
@@ -1,7 +1,31 @@
-const CircularLoader = () => {
-  // ToDo: Add Size variants
+import { Size } from "../../../utils/enums";
+
+interface CircularLoaderProps {
+  size?: Size;
+  className?: string;
+}
+
+const CircularLoader = ({ size = Size.md, className = "" }: CircularLoaderProps) => {
+  const sizeClasses = {
+    [Size.sm]: "h-6 w-6 border-2",
+    [Size.md]: "h-8 w-8 border-4",
+    [Size.lg]: "h-12 w-12 border-4",
+  };
+
   return (
-    <div className="h-full min-h-6 max-h-32 aspect-square border-4 rounded-full border-t-4 border-t-[currentColor] m-auto animate-spin"></div>
+    <div
+      className={`
+        ${sizeClasses[size]} 
+        aspect-square 
+        rounded-full 
+        border-t-[currentColor] 
+        m-auto 
+        animate-spin
+        ${className}
+      `
+        .trim()
+        .replace(/\s+/g, " ")}
+    />
   );
 };
 
