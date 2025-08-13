da1805bd087ec6093b4a36a2e3f81d51c46c7b58
Mohd Imtiaz <imtiyaz.masodi@gmail.com>
2025-08-13 20:07:20 +0530

    Added UpdateProfileRequestType

---
+++ client/src/types/UpdateProfileRequestType.ts
@@ -0,0 +1,7 @@
+// Types for update profile request
+export interface UpdateProfileRequestType {
+  name?: string;
+  bio?: string;
+  location?: string;
+  dateOfBirth?: string; // ISO date string
+}
