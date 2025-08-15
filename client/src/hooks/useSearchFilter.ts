4a2d5ff38017d7b1aa8f4ed04b4618496df0cbb7
Mohd Imtiaz <imtiyaz.masodi@gmail.com>
2025-08-15 13:24:44 +0530

    Added SearchFilter Hook

---
+++ client/src/hooks/useSearchFilter.ts
@@ -0,0 +1,12 @@
+import { useContext } from "react";
+import { SearchFilterContext } from "../contexts/SearchFilterContext";
+
+export type { FilterOptions, SearchFilterState } from "../contexts/SearchFilterContext";
+
+export const useSearchFilter = () => {
+  const context = useContext(SearchFilterContext);
+  if (!context) {
+    throw new Error("useSearchFilter must be used within a SearchFilterProvider");
+  }
+  return context;
+};
