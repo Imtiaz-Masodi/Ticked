a7a1b2fcf9185a9396fa43b709340450ebbdf2f9
Mohd Imtiaz <imtiyaz.masodi@gmail.com>
2025-09-03 13:26:21 +0530

    Added xs size to CircularLoader component

--- client/src/components/Loader/CircularLoader/CircularLoader.tsx
+++ client/src/components/Loader/CircularLoader/CircularLoader.tsx
@@ -1,12 +1,13 @@
 import { Size } from "../../../utils/enums";
 
 interface CircularLoaderProps {
-  size?: Size;
+  size?: Size | "xs";
   className?: string;
 }
 
 const CircularLoader = ({ size = Size.md, className = "" }: CircularLoaderProps) => {
   const sizeClasses = {
+    xs: "h-4 w-4 border",
     [Size.sm]: "h-6 w-6 border-2",
     [Size.md]: "h-8 w-8 border-4",
     [Size.lg]: "h-12 w-12 border-4",
